00c964b5b5625e72551a51492cb2f346ed634c37
Grizzly <grizzlyxyz@gmail>
2019-01-17 09:27:49 +0100

    conditon

---
+++ style.js
@@ -0,0 +1,31 @@
+// Exo 1
+// Stockez dans une variable "taille_nom_guerrier" la longueur du nom du guerrier
+// Stockez dans une variable "troiseme_index_arme_guerrier" l'index 3 de l'arme du guerrier
+// Stockez dans une variable "morceau_arme_mage" le nom de l'arme du guerrier a partir de l'index 2
+// Stockez dans une variable "fusion_noms_silvie_nico" le nom du guerrier concaténé au nom du paysanlet guerrier = {
+    nom : "nico",
+    age : 18,
+    taille : 180,
+    arme : "hache",
+   };
+   let mage = {
+    nom : "hajar",
+    age : 20,
+    taille : 160,
+    arme : "baton",
+   };
+   let paysan = {
+    nom : "sylvie",
+    age : 17,
+    taille : 165,
+    arme : "fourche",
+   };
+   // Exo 2
+   // Créez une variable objet du nom de perso2 avec comme proprietées un nom et un age
+   // Créez une variable objet du nom de perso1 avec comme propriété un nom, un age et comme method "usurpation"
+   // Il doit pouvoir usurper l'identité du perso2
+   // Exo 3
+   // Créer un objet avec un nom et une avec une method
+   // La method de votre objet lance un prompt permetant de changer son age
+   // Une alert renvoi l"age de votre objet avec la phrase "Vous avez x ans"
+  
